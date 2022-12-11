b811970e21043aacd1ea01dfa4628423be215e09
Nepolean23 <nlean1996@gmail.com>
2022-12-11 18:06:34 +0530

    Code For Adding Numbers In BinarySearchTree

---
+++ BinarySearchTree/BinarySearchTree/BinarySearch.cs
@@ -0,0 +1,10 @@
+﻿namespace BinarySearchTree
+{
+    public class UC_1_AbiltyToCreateBST_AddingNumbers<T> where T : IComparable<T>
+    {
+        internal void Insert<T>(T item) where T : IComparable<T>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BinarySearchTree/BinarySearchTree/UC_1-AbiltyToCreateBST_AddingNumbers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class UC_1_AbiltyToCreateBST_AddingNumbers<T> where T : IComparable<T>
+    { 
+        public T NodeData { get; set; }
+        public UC_1_AbiltyToCreateBST_AddingNumbers <T> leftTree { get; set; }
+        public UC_1_AbiltyToCreateBST_AddingNumbers <T> rightTree { get; set; }
+
+        public UC_1_AbiltyToCreateBST_AddingNumbers(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.leftTree = leftTree;
+            this.rightTree = rightTree;
+        }
+        int leftcount=0, rightcount=0;
+        bool result=false;
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if(currentNodeValue.CompareTo(item) > 0 ) 
+            {
+                if (this.leftTree == null)
+                    this.leftTree = new UC_1_AbiltyToCreateBST_AddingNumbers<T>(item);
+                else
+                    this.leftTree.Insert(item);
+
+                
+            }
+            else 
+            {
+                if (this.rightTree == null)
+                    this.rightTree = new UC_1_AbiltyToCreateBST_AddingNumbers<T>(item);
+                else
+                    this.rightTree.Insert(item);
+            }
+        }
+
+
+
+         
+
+
+    }
+}
