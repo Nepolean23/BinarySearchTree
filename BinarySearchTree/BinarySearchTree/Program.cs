ab7f265d7020ca6df0f93d772475ebb0b27406f9
Nepolean23 <nlean1996@gmail.com>
2022-12-11 20:33:01 +0530

    Code For Creating BST-CheckingSize

--- BinarySearchTree/BinarySearchTree/Program.cs
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using BinarySearch;
+using BinarySearchTree;
+
+//UC_1_AbiltyToCreateBST_AddingNumbers<int> BST1 = new UC_1_AbiltyToCreateBST_AddingNumbers<int>(56);
+//BST1.Insert(30);
+//BST1.Insert(70);
+UC_2_AbilityToCreateBST_CheckingSize<int> BST2 = new UC_2_AbilityToCreateBST_CheckingSize<int>(56);
+BST2.Insert(30);
+BST2.Insert(70);
+BST2.Display();
---
+++ BinarySearchTree/BinarySearchTree/UC_2-AbilityToCreateBST_CheckingSize.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    public class UC_2_AbilityToCreateBST_CheckingSize<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+
+        public UC_2_AbilityToCreateBST_CheckingSize<T> leftTree { get; set; }
+        public UC_2_AbilityToCreateBST_CheckingSize<T> rightTree { get; set; }
+
+        public UC_2_AbilityToCreateBST_CheckingSize( T NodeData)
+        {
+            this.NodeData = NodeData;
+            this.leftTree = null;
+            this.rightTree = null;
+        }
+
+        int leftcount=0, rightcount=0;
+        bool result=false;
+
+        public void Insert(T item)
+        {
+            T currentvalue = this.NodeData;
+            if (currentvalue.CompareTo(NodeData) > 0 ) 
+            {
+                if (leftTree == null)
+                    leftTree = new UC_2_AbilityToCreateBST_CheckingSize<T>(item);
+                else
+                    leftTree.Insert(item);
+
+            }
+            else
+            {
+                if(rightTree==null)
+                    rightTree=new UC_2_AbilityToCreateBST_CheckingSize<T>(item);
+                else
+                    rightTree.Insert(item);
+            }
+        }
+         public void Display()
+         {
+            if (this.leftTree != null)
+            {
+                leftcount++;
+                leftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if(this.rightTree!=null)
+            {
+                rightcount++;
+                rightTree.Display();
+            }
+         }
+         public void GetSize()
+         {
+            Console.WriteLine("Size"+" "+(1+this.leftcount+this.rightcount));
+         }
+
+    }
+}
