2e48631a48cb97c7d97bef793e7fecc40a86fa1a
Nepolean23 <nlean1996@gmail.com>
2022-12-11 21:30:23 +0530

    Code For Bst to Search

--- BinarySearchTree/BinarySearchTree/Program.cs
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -4,7 +4,17 @@ using BinarySearchTree;
 //UC_1_AbiltyToCreateBST_AddingNumbers<int> BST1 = new UC_1_AbiltyToCreateBST_AddingNumbers<int>(56);
 //BST1.Insert(30);
 //BST1.Insert(70);
-UC_2_AbilityToCreateBST_CheckingSize<int> BST2 = new UC_2_AbilityToCreateBST_CheckingSize<int>(56);
-BST2.Insert(30);
-BST2.Insert(70);
-BST2.Display();
+
+//UC_2_AbilityToCreateBST_CheckingSize<int> BST2 = new UC_2_AbilityToCreateBST_CheckingSize<int>(56);
+//BST2.Insert(30);
+//BST2.Insert(70);
+//BST2.Display();
+
+UC_3_AbilityToCreateBST_Search<int> BST3 = new UC_3_AbilityToCreateBST_Search<int>(56);
+BST3.Insert(30);
+BST3.Insert(70);
+BST3.Insert(63);
+BST3.Display();
+bool result = BST3.IfExists(63,BST3 );
+Console.WriteLine(result);
+Console.ReadKey();
---
+++ BinarySearchTree/BinarySearchTree/UC_3-AbilityToCreateBST_Search.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    public class UC_3_AbilityToCreateBST_Search<T> where T:IComparable<T>
+    {
+        public T NodeData { get; set; }
+
+        public UC_3_AbilityToCreateBST_Search<T> leftTree { get; set; }
+        public UC_3_AbilityToCreateBST_Search<T> rightTree { get; set; }
+
+        public UC_3_AbilityToCreateBST_Search(T nodeData)
+        {
+            this.NodeData = nodeData;
+            this.leftTree = null;
+            this.rightTree = null;
+        }
+
+        int leftcount=0, rightcount=0;
+        bool result = false;
+
+        public void Insert(T item)
+        {
+            T Currentvalue = this.NodeData;
+             if(Currentvalue.CompareTo(this.NodeData)> 0 ) 
+             {
+                if(this.leftTree == null)
+                this.leftTree= new UC_3_AbilityToCreateBST_Search<T>(item);
+                else
+                    this.leftTree.Insert(item);
+             }
+             else
+             {
+                if (this.righTree == null)
+                    this.righTree = new UC_3_AbilityToCreateBST_Search<T>(item);
+                else
+                    this.leftTree.Insert(item);
+             }
+        }
+        public void Display()
+        {
+            if(leftTree!=null)
+            {
+                this.leftcount++;
+                this.leftTree.Display();
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if(this.rightTree!=null)
+            {
+                this.rightcount++;
+                this.rightTree.Display();
+            }
+        }
+        public void GetSize()
+        {
+            Console.WriteLine("Size"+" "+(1+this.leftcount+this.rightcount));
+
+        }
+        public bool IfExists(T element, UC_3_AbilityToCreateBST_Search<T> node)
+        {
+            if (node==null)
+                return false;
+            if(node.NodeData.Equals(element))
+            {
+                Console.WriteLine("Found the element in BST"+" "+node.NodeData);
+                result = true;
+            }
+            else
+            
+                Console.WriteLine("The current element is {0}",node.NodeData);
+                if (element.CompareTo(NodeData) < 0)
+                    IfExists(element, node.leftTree);
+                if(element.CompareTo(NodeData)>0)
+                    IfExists(element, node.rightTree);
+                return result;
+
+            
+        }
+    }
+}
